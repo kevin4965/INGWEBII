dd2cab120a67ce5061ce75aa7bc6254805f33c6b
jorgejc <jorgejc25@gmail.com>
2024-09-05 20:48:15 -0500

    Se crean los servicios de listar y actualizar inventarios

--- router/inventario.js
+++ router/inventario.js
@@ -55,4 +55,91 @@ router.post('/', [
     }
 })
 
+
+// Listar inventarios
+router.get('/', async function (req, res) {
+    
+    try {
+
+        const inventarios = await Inventario.find().populate([
+            {
+                path: 'usuario', select: 'nombre email estado'
+            },
+            {
+                path: 'marca', select: 'nombre estado'
+            },
+            {
+                path: 'estadoEquipo', select: 'nombre estado'
+            },
+            {
+                path: 'tipoEquipo', select: 'nombre estado'
+            }
+
+        ]);
+
+        res.send(inventarios);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrió un error')
+    }
+    
+  });
+
+  router.put('/:inventarioId', [
+    check('serial', 'invalid.serial').not().isEmpty(),
+    check('modelo', 'invalid.modelo').not().isEmpty(),
+    check('descripcion', 'invalid.descripcion').not().isEmpty(),
+    check('color', 'invalid.color').not().isEmpty(),
+    check('foto', 'invalid.foto').not().isEmpty(),
+    check('fechaCompra', 'invalid.fechaCompra').not().isEmpty(),
+    check('precio', 'invalid.precio').not().isEmpty(),
+    check('marca', 'invalid.marca').not().isEmpty(),
+    check('estadoEquipo', 'invalid.estadoEquipo').not().isEmpty(),
+    check('tipoEquipo', 'invalid.tipoEquipo').not().isEmpty(),
+
+], async function(req, res){
+
+    try {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ mensaje: errors.array() });
+        }
+
+        let inventario = await Inventario.findById(req.params.inventarioId);
+        if (!inventario) {
+            return res.status(400).sed('Inventario no existe')
+        }
+
+        const existeInventarioPorSerial = await Inventario.findOne({ serial: req.body.serial, _id:{ $ne: inventario._id} });
+        if (existeInventarioPorSerial) {
+            return res.status(400).send('Ya existe el serial para otro equipo ')
+        }
+
+
+        inventario.serial = req.body.serial;
+        inventario.modelo = req.body.modelo;
+        inventario.descripcion = req.body.descripcion;
+        inventario.color = req.body.color;
+        inventario.foto = req.body.foto;
+        inventario.fechaCompra = req.body.fechaCompra;
+        inventario.precio = req.body.precio;
+        inventario.usuario = req.body.usuario._id;
+        inventario.marca = req.body.marca._id;
+        inventario.estadoEquipo = req.body.estadoEquipo._id;
+        inventario.tipoEquipo = req.body.tipoEquipo._id;
+        inventario.fechaActualizacion = new Date();
+
+        inventario = await inventario.save();
+        res.send(inventario);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrió un error al actualizar inventario');
+        
+    }
+})
+
+
 module.exports = router
